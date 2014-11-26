88b2a4d4ed8d4de7c52eef8eea85ce2f2b09230a
Oliver Pope <opope@me.com>
2014-11-25 23:46:41 -0500

    updates. you know the usual

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,61 @@
 */
 
 /* gulp and plugins ----------------------------------------- */
-var gulp 				= require('gulp'),
-		stylus 			= require('gulp-stylus'),
-		cssmin			= require('gulp-cssmin'),
-		rename			= require('gulp-rename'),
-		watch				= require('gulp-watch'),
-		webserver 	= require('gulp-webserver');
+var gulp 								= require('gulp'),
+		stylus 							= require('gulp-stylus'),
+		cssmin							= require('gulp-cssmin'),
+		rename							= require('gulp-rename'),
+		watch								= require('gulp-watch'),
+		webserver 					= require('gulp-webserver'),
+		jade 								= require('gulp-jade'),
+		prettify 						= require('gulp-prettify'),
+		autoprefixer 				= require('gulp-autoprefixer'),
+		concat 							= require('gulp-concat'),
+		del 								= require('del');
 
 /* destinations -------------------------------------------- */
-var stylusFile 	= './stylus/style.styl',
-		cssFile			= './css/style.css';
+var stylusFile 					= './stylus/*.styl',
+		jadeFile						= './pages/*.jade',
+		dist								= './dist';
 
 /* stylus to css ------------------------------------------- */
 gulp.task('stylus', function() {
 	gulp.src(stylusFile)
 		.pipe(stylus())
-		.pipe(gulp.dest('./css'));
-});
-
-/* compress css -------------------------------------------- */
-gulp.task('compress', function() {
-	gulp.src(cssFile)
+		.pipe(autoprefixer({
+				browsers: ['last 2 versions'],
+				cascade: true
+		}))
+		.pipe(gulp.dest(dist))
+		.pipe(concat('all.css'))
+		.pipe(gulp.dest(dist))
 		.pipe(cssmin())
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest(dist));
 });
 
 /* webserver ----------------------------------------------- */
 gulp.task('webserver', function() {
 	gulp.src('./')
 		.pipe(webserver({
+			livereload: true,
+			directoryListing: false,
 			open: true,
 			port: 9000
 		}));
+});
+
+/* jade to html -------------------------------------------- */
+gulp.task('jade', function() {
+	gulp.src(jadeFile)
+		.pipe(jade())
+		.pipe(prettify({indent_size: 2}))
+		.pipe(gulp.dest('./'));
+});
+
+/* clean build ---------------------------------------------- */
+gulp.task('clean', function (cb) {
+	del(dist, cb);
 });
 
 /* default task -------------------------------------------- */
@@ -46,6 +68,8 @@ gulp.task('default', function() {
 	gulp.start('webserver');
 	watch(stylusFile, function(files, cb) {
 		gulp.start('stylus', cb);
-		gulp.start('compress', cb);
 	});
+	watch(jadeFile, function(files, cb) {
+		gulp.start('jade', cb);
+	})
 });
